e2be709174714550309eb6c43e97522c269eda6e
Filip Harvancik <filipharvancik@gmail.com>
2026-05-11 20:10:48 +0200

    Dlinear cez CPU

--- Models/LTSF-Linear/run_longExp.py
+++ Models/LTSF-Linear/run_longExp.py
@@ -98,7 +98,7 @@ def main():
         '--freq',
         required=False,
         type=str,
-        default='h',
+        default='b',
         help='freq for time features encoding, options:[s:secondly, t:minutely, h:hourly, d:daily, b:business days, w:weekly, m:monthly], you can also use more detailed freq like 15min or 3h',
     )
     parser.add_argument('--checkpoints', required=False, type=str, default='./checkpoints/', help='location of model checkpoints')
@@ -151,7 +151,7 @@ def main():
     parser.add_argument('--num_workers', type=int, default=10, help='data loader num workers')
     parser.add_argument('--itr', type=int, default=1, help='experiments times')
     parser.add_argument('--train_epochs', type=int, default=100, help='train epochs')
-    parser.add_argument('--batch_size', type=int, default=32, help='batch size of train input data')
+    parser.add_argument('--batch_size', type=int, default=8, help='batch size of train input data')
     parser.add_argument('--patience', type=int, default=5, help='early stopping patience')
     parser.add_argument('--learning_rate', type=float, default=0.01, help='optimizer learning rate')
     parser.add_argument('--des', type=str, default='Exp', help='exp description')
@@ -184,6 +184,7 @@ def main():
     np.random.seed(fix_seed)
 
     args.use_gpu = True if torch.cuda.is_available() and args.use_gpu else False
+    args.use_gpu = False
 
     if args.use_gpu and args.use_multi_gpu:
         args.devices = args.devices.replace(' ', '')
